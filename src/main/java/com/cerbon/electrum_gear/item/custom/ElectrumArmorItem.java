f32271520e1bc040da21d50d44375f0e76c3c8e9
Felipe <felipecerboncinix@gmail.com>
2024-02-09 16:11:51 -0300

    Electrum armor makes piglins neutral

--- src/main/java/com/cerbon/electrum_gear/item/custom/ElectrumArmorItem.java
+++ src/main/java/com/cerbon/electrum_gear/item/custom/ElectrumArmorItem.java
@@ -1,5 +1,7 @@
 package com.cerbon.electrum_gear.item.custom;
 
+import com.cerbon.electrum_gear.item.EGArmorMaterials;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
@@ -9,6 +11,14 @@ public class ElectrumArmorItem extends ArmorItem {
 
     public ElectrumArmorItem(ArmorMaterial material, Type type, Properties properties) {
         super(material, type, properties);
+    }
+
+    @Override
+    public boolean makesPiglinsNeutral(ItemStack stack, LivingEntity wearer) {
+        if (stack.getItem() instanceof ArmorItem armorItem && armorItem.getMaterial() == EGArmorMaterials.ELECTRUM)
+            return true;
+
+        return super.makesPiglinsNeutral(stack, wearer);
     }
 
     @Override
