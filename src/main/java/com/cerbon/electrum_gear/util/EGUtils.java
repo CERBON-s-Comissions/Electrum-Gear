753e10128de7db66dd3a6961947c5e831a0aa627
Felipe <felipecerboncinix@gmail.com>
2024-02-08 21:13:10 -0300

    Improve util methods

--- src/main/java/com/cerbon/electrum_gear/util/EGUtils.java
+++ src/main/java/com/cerbon/electrum_gear/util/EGUtils.java
@@ -8,26 +8,11 @@ import net.minecraft.world.item.ItemStack;
 public class EGUtils {
 
     public static boolean hasFullSuitOfArmorOn(Player player) {
-        ItemStack helmet = player.getInventory().getArmor(3);
-        ItemStack chestPlate = player.getInventory().getArmor(2);
-        ItemStack leggings = player.getInventory().getArmor(1);
-        ItemStack boots = player.getInventory().getArmor(0);
-
-        return !helmet.isEmpty() && !chestPlate.isEmpty() &&
-               !boots.isEmpty()  && !leggings.isEmpty();
+        return player.getInventory().armor.stream().noneMatch(ItemStack::isEmpty);
     }
 
     public static boolean hasCorrectArmorOn(ArmorMaterial material, Player player) {
-        for (ItemStack armor : player.getInventory().armor)
-            if (!(armor.getItem() instanceof ArmorItem)) return false;
-
-        ArmorItem helmet = ((ArmorItem) player.getInventory().getArmor(3).getItem());
-        ArmorItem chestPlate = ((ArmorItem) player.getInventory().getArmor(2).getItem());
-        ArmorItem leggings = ((ArmorItem) player.getInventory().getArmor(1).getItem());
-        ArmorItem boots = ((ArmorItem) player.getInventory().getArmor(0).getItem());
-
-        return helmet.getMaterial() == material && chestPlate.getMaterial() == material &&
-               boots.getMaterial() == material && leggings.getMaterial() == material;
+        return player.getInventory().armor.stream().allMatch(armorStack -> armorStack.getItem() instanceof ArmorItem armorItem && armorItem.getMaterial() == material);
     }
 
     public static boolean hasAnyCorrectArmonOn(ArmorMaterial material, Player player) {
